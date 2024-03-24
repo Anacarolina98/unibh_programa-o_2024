731839c46e39eab00a52dd004b47542029cc5aa5
Ana Carolina Costa Barbosa <101832024+Anacarolina98@users.noreply.github.com>
2024-03-24 19:31:20 -0300

    Update Main.java

---
+++ Main.java
@@ -0,0 +1,24 @@
+// import org.junit.jupiter.api.Test;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        boolean notaValida = false;
+        boolean notaInvalda = true;
+        do {
+            System.out.print("Digite uma nota: ");
+            double nota = scan.nextDouble();
+
+            if (nota >=0 && nota <= 10){
+                notaValida = true;
+                System.out.println("Você digitou: " + nota);
+            } else {
+                System.out.println("Nota inválida, digite novamente.");
+            }
+
+        } while (!notaValida);
+    }
+}
